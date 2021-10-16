5c988784d44272388d654c9ce53f47dc71bd8745
Jan Janssens <jan.janssens@digiti.be>
2021-10-16 20:54:57 +0200

    test run

--- src/Http/Controllers/TemplateController.php
+++ src/Http/Controllers/TemplateController.php
@@ -2,7 +2,7 @@
 
 namespace TheRealJanJanssens\Pakka\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+//use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Storage;
 use TheRealJanJanssens\Pakka\Models\Template;
